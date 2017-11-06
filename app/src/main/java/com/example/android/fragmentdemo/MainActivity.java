71a1440f3f2877afd43ab9625ab79fb65b49ee46
giyoon21c <giyoon21c@gmail.com>
2017-11-05 18:49:44 -0800

    dynamic flagment creation

--- app/src/main/java/com/example/android/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentdemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.fragmentdemo;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    private void addFragment() {
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SampleFragment sampleFragment = new SampleFragment();
+        fragmentTransaction.add(R.id.fragmentContainer, sampleFragment);
+        fragmentTransaction.commit();
     }
 }
